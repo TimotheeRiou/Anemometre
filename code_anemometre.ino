2c6e673ac052d32c22ce4ddd855ac7f4963a75c9
Timothée Riou <60222684+TimotheeRiou@users.noreply.github.com>
2020-01-27 10:30:06 +0100

    Add files via upload

---
+++ code_anemometre.ino
@@ -0,0 +1,46 @@
+#include <Servo.h>
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+
+Servo myservo;
+
+int compteurfm;
+int nombreTour = 0;
+int temps = 1;
+unsigned long compte_temps;
+float x;
+
+void setup()
+{
+  lcd.begin(16,2);
+  Serial.begin(9600);
+  pinMode (2,INPUT_PULLUP);
+  attachInterrupt (0, anemometre, RISING);
+  myservo.attach(4);
+  myservo.write(0);
+}
+
+void loop()
+{
+  x = 2*3.1415*0.05*5*compteurfm/8;
+  lcd.setCursor(4,1);
+  lcd.print(x);
+  lcd.println ("m/s          ");
+  compteurfm = 0;
+  delay(1000);
+  lcd.clear();
+}
+
+void anemometre()
+{
+  compteurfm++;
+  
+  if(x>=11){
+    myservo.write(90);
+  }else{
+    myservo.write(1);
+  }
+}
+
